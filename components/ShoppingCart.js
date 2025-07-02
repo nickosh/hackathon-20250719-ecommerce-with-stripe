62017b40ff5b5ad5b89be4cf924e52ea78c10d0a
dandk105 <d2cak5s.e43d@gmail.com>
2025-07-02 20:56:08 +0900

    add sample

--- components/ShoppingCart.js
+++ components/ShoppingCart.js
@@ -1,11 +1,28 @@
 import { useShoppingCart } from "use-shopping-cart";
 import CartItem from "./CartItem";
 import CheckoutButton from "./CheckoutButton";
+import {useEffect, useRef} from "react";
 
 export default function ShoppingCart() {
-  const { shouldDisplayCart, cartCount, cartDetails } = useShoppingCart();
+  const { shouldDisplayCart, cartCount, cartDetails, handleCartClick } = useShoppingCart();
+  const cartRef = useRef(null);
+
+  // ポップアップ外をクリックした際にカートを閉じる
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (cartRef.current && !cartRef.current.contains(event.target)) {
+        handleCartClick(false); // カートを閉じる
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [handleCartClick]);
   return (
     <div
+        ref={cartRef}
       className={`bg-white flex flex-col absolute right-3 md:right-9 top-14 w-80 py-4 px-4 shadow-[0_5px_15px_0_rgba(0,0,0,.15)] rounded-md transition-opacity duration-500 ${
         shouldDisplayCart ? "opacity-100" : "opacity-0"
       }`}
